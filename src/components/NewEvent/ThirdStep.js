94270e1ab832b2f11dcd9e0eb3b0c4957aaedb3c
gabrielsclimaco <gabrielsclimaco@gmail.com>
2018-01-30 10:19:13 -0200

    ✨ Add checkbox

--- src/components/NewEvent/ThirdStep.js
+++ src/components/NewEvent/ThirdStep.js
@@ -1,48 +1,81 @@
-import React from 'react';
+import React, { Component } from 'react';
+import MaterialIcon from 'material-icons-react';
 import { Card } from '../Card';
 import './ThirdStep.css';
 
-function ThirdStep(props) {
-  const fields = [
-    {placeholder: 'CEP', attribute: 'cep'},
-    {placeholder: 'Local*', attribute: 'place'},
-    {placeholder: 'Avenida/Rua*', attribute: 'street'},
-    {placeholder: 'Número', attribute: 'number'},
-    {placeholder: 'Complemento', attribute: 'complement'},
-    {placeholder: 'Bairro', attribute: 'district'},
-  ]
+class ThirdStep extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      fields: [
+        {placeholder: 'CEP', attribute: 'cep'},
+        {placeholder: 'Local*', attribute: 'place'},
+        {placeholder: 'Avenida/Rua*', attribute: 'street'},
+        {placeholder: 'Número', attribute: 'number'},
+        {placeholder: 'Complemento', attribute: 'complement'},
+        {placeholder: 'Bairro', attribute: 'district'},
+      ],
+      showOnMaps: false,
+    };
+
+    this.renderInputs = this.renderInputs.bind(this);
+    this.toggleState = this.toggleState.bind(this)
+  }
   
-  const renderInputs = () => {
-    return fields.map((field) => {
+  renderInputs() {
+    return this.state.fields.map((field) => {
       return (
         <input
-          onChange={e => {props.onFieldTyped({
+          onChange={e => {this.props.onFieldTyped({
             value: e.target.value,
             attribute: field.attribute
           })}}
           placeholder={field.placeholder}
           className='ThirdStep-formTitleInput'
+          key={field.attribute}
         />
       );
     });
   }
 
-  return (
-    <Card className={props.className}>
-      <div className='ThirdStep'>
-        <span className='ThirdStep-number'>3</span>
-        <div>
-          <span className='ThirdStep-title'>ONDE?</span>
-          <div className='ThirdStep-form'>
-            <span>Cadastrar endereço*</span>
-            <div>
-              {renderInputs()}
+  toggleState() {
+    this.setState({showOnMaps: !this.state.showOnMaps});
+  }
+
+  render() {
+    return (
+      <Card className={this.props.className}>
+        <div className='ThirdStep'>
+          <span className='ThirdStep-number'>3</span>
+          <div>
+            <span className='ThirdStep-title'>ONDE?</span>
+            <div className='ThirdStep-form'>
+              <span>Cadastrar endereço*</span>
+              <div>
+                { this.renderInputs() }
+              </div>
+            </div>
+            <div className='ThirdStep-mapCheckboxSection'>
+              <span
+                className='ThirdStep-mapCheckbox'
+                onClick={this.toggleState}>
+                <input type='checkbox' checked={this.state.showOnMaps} />
+                <span>
+                  <MaterialIcon
+                    icon='check'
+                    size={20}
+                    color='white'
+                  />
+                </span>
+              </span>
+              <span>Vizualizar no Google Maps</span>
             </div>
           </div>
         </div>
-      </div>
-    </Card>
-  );
+      </Card>
+    );
+  }
 }
 
 export default ThirdStep;
